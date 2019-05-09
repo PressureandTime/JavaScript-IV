d164e6b1b1f7b083784e1c189c46d2dabf8a09d4
Petar Scepanovic <hiborija@gmail.com>
2019-05-09 15:35:56 +0200

    created all the classes and subclasses, going to test it

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -23,3 +23,97 @@ class Person {
   }
 
 }
+
+
+
+// Now that we have a Person as our base class, we'll build our Instructor class.
+// Instructor uses the same attributes that have been set up by Person
+// Instructor has the following unique props:
+// specialty what the Instructor is good at i.e. 'redux'
+// favLanguage i.e. 'JavaScript, Python, Elm etc.'
+// catchPhrase i.e. Don't forget the homies
+// Instructor has the following methods:
+// demo receives a subject string as an argument and logs out the phrase
+// 'Today we are learning about {subject}' where subject is the param passed in.
+// grade receives a student object and a subject string as arguments
+// and logs out '{student.name} receives a perfect score on {subject}'
+
+
+
+class Instructor extends Person {
+  constructor(instructorAttributes) {
+    super(instructorAttributes);
+    this.specialty = instructorAttributes.specialty;
+    this.favLanguage = instructorAttributes.favLanguage;
+    this.catchPhrase = instructorAttributes.catchPhrase;
+  }
+
+  demo() {
+    return `Today we are learning about ${this.favLanguage} and ${this.name}`;
+  }
+}
+
+
+
+
+// Now we need some students!
+// Student uses the same attributes that have been set up by Person
+// Student has the following unique props:
+// previousBackground i.e. what the Student used to do before Lambda School
+// className i.e. CS132
+// favSubjects. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// Student has the following methods:
+// listsSubjects a method that logs out all of the student's favoriteSubjects one by one.
+// PRAssignment a method that receives a subject as an argument and logs out that the student.name has submitted a PR for {subject}
+// sprintChallenge similar to PRAssignment but logs out student.name has begun sprint challenge on {subject}
+
+
+
+class Student extends Person {
+  constructor(studentAttributes) {
+    super(studentAttributes);
+    this.previousBackground = studentAttributes.previousBackground;
+    this.className = studentAttributes.className;
+    this.favSubjects = studentAttributes.favSubjects;
+  }
+
+  listsSubjects() {
+    return `all of the favorite subjects are ${this.favSubjects}`;
+  }
+
+  praAssignment() {
+    return `Student ${this.name} has submitted a PR for `
+  }
+
+  sprintChallenge() {
+    return `Student ${this.name} has begun sprint challenge on subject `
+  }
+}
+
+
+
+// Now that we have instructors and students, we'd be nowhere without our PM's
+// ProjectManagers are extensions of Instructors
+// ProjectManagers have the following unique props:
+// gradClassName: i.e. CS1
+// favInstructor: i.e. Sean
+// ProjectManagers have the following Methods:
+// standUp a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+// debugsCode a method that takes in a student object and a subject and logs out {name} debugs {student.name}'s code on {subject}
+
+class ProjectManagers extends Instructor {
+  constructor(pmAttributes) {
+    super(pmAttributes);
+    this.gradClassName = pmAttributes.gradClassName;
+    this.favInstructor = pmAttributes.favInstructor;
+  }
+
+  standUp() {
+    return `${this.name} announces to channel`;
+  }
+
+  debugsCode() {
+    return `${this.name} debugs ${this.name} code on subject `
+  }
+
+}
